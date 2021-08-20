e03384b1346e31f9f2952c54af4de89b122fd52d
leo.lambert02@gmail.com <leo.lambert02@gmail.com>
2021-08-20 19:28:14 +0200

    Added config file

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config")
 
 const app = express()
+
+const mongoUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+mongoose
+    .connect(mongoUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    })
+    .then(() => console.log("Successfully connected to database"))
+    .catch((e) => console.log(e))
+
+
 
 app.get("/", (req, res) => {
     res.send(`<h1>Db status ${e}<h1/>`)
