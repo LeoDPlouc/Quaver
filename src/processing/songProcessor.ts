046bac3a72569d41522045c60d0d795a23b5fbb5
leo.lambert02@gmail.com <leo.lambert02@gmail.com>
2022-02-05 19:24:20 +0100

    Changed fpcalc to an async version

--- src/processing/songProcessor.ts
+++ src/processing/songProcessor.ts
@@ -1,32 +1,30 @@
-    // Quaver is a self-hostable music player and music library manager
-    // Copyright (C) 2022  DPlouc
+// Quaver is a self-hostable music player and music library manager
+// Copyright (C) 2022  DPlouc
 
-    // This program is free software: you can redistribute it and/or modify
-    // it under the terms of the GNU General Public License as published by
-    // the Free Software Foundation, either version 3 of the License, or
-    // (at your option) any later version.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 
-    // This program is distributed in the hope that it will be useful,
-    // but WITHOUT ANY WARRANTY; without even the implied warranty of
-    // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    // GNU General Public License for more details.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 
-    // You should have received a copy of the GNU General Public License
-    // along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import { parseFile } from "music-metadata"
 import { Document } from "mongoose"
 import Path from "path"
-import fpcalc from "fpcalc"
-import { promisify } from "util"
+import fp from "fpcalc-async"
 
 import { Album, IAlbum } from "../models/albumModel"
 import { Artist, IArtist } from "../models/artistModel"
 import { ISong } from "../models/songModel"
 import { FPCALC_PATH } from "../config/config"
 
-async function getAcoustid(songPath: string): Promise<string> {
-    const fp = promisify(fpcalc)
+export async function getAcoustid(songPath: string): Promise<string> {
     var fingerprint
 
     //If fpcalc isn't in PATH, use fpcalc with its path
