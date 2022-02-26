b6f7ee48eb939d616234ea2e5f1c87d9b1afaf45
leo.lambert02@gmail.com <leo.lambert02@gmail.com>
2022-02-26 20:13:57 +0100

    made scripts fail if one of the cmd fails

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,17 +1,17 @@
-rm -vr build
-rm -vr dist
-
-mkdir -v build
-mkdir -v dist
-
-tsc -b --verbose
-
-mv -v ./build/* dist/
-
-npx webpack --config webpack.config.js 
-mkdir -v dist/src
-mv -v ./build/ dist/src/js
-cp -v ./front/index.html ./dist/src
-cp -vr ./front/img ./dist/src
-cp -v ./changelog.txt ./dist/changelog.txt
+rm -vr build &&\
+rm -vr dist &&\
+\
+mkdir -v build &&\
+mkdir -v dist &&\
+\
+tsc -b --verbose &&\
+\
+mv -v ./build/* dist/ &&\
+\
+npx webpack --config webpack.config.js && \
+mkdir -v dist/src &&\
+mv -v ./build/ dist/src/js &&\
+cp -v ./front/index.html ./dist/src &&\
+cp -vr ./front/img ./dist/src &&\
+cp -v ./changelog.txt ./dist/changelog.txt &&\
 cp -v ./LICENSE ./dist/LICENCE
