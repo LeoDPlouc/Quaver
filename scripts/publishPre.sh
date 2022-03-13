0943963b428642c46299f04c58e25928fa012f29
leo.lambert02@gmail.com <leo.lambert02@gmail.com>
2022-03-13 17:21:33 +0100

    edit publication script

--- scripts/publishPre.sh
+++ scripts/publishPre.sh
@@ -1,5 +1,5 @@
 npm run build &&\
 \
-docker build -t dplouc/quaver:$1 -t dplouc/quaver:pre:latest . &&\
+docker build -t dplouc/quaver:$1 -t dplouc/quaver-pre:latest . &&\
 \
-docker push dplouc/quaver:pre:latest
+docker push dplouc/quaver-pre:latest
