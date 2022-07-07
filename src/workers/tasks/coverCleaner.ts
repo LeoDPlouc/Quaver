5c26fa56dc07c5995eef973dfdb9d76f8b8a181d
leo.lambert02@gmail.com <leo.lambert02@gmail.com>
2022-07-07 20:41:27 +0200

    Added cover cleaner : - Cleans images with dead files - Cleans image without tiny image file - Clean album's coverId - Cleans images in IMAGE_PATH that are not used in cover models - Cleans cover without tiny image

--- src/workers/tasks/coverCleaner.ts
+++ src/workers/tasks/coverCleaner.ts
@@ -11,38 +11,207 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import { IMAGES_PATH } from "../../config/config";
 import { albumService } from "../../service/albumService";
+import { fileService } from "../../service/fileService";
 import { imageService } from "../../service/imageService";
+import { createFailure } from "../../utils/Failure";
 import { logError, logInfo } from "../../utils/logger";
 
-let images: Image[] = [];
-let albums: Album[] = [];
-
 async function cleanAlbumlessImages() {
+  try {
+    var images = await imageService.getAllImages();
+    var albums = await albumService.getAllAlbums();
+  } catch (err) {
+    throw createFailure(
+      "DAO error",
+      __filename,
+      cleanAlbumlessImages.name,
+      err
+    );
+  }
+
   for (let i = 0; i < images.length; i++) {
     if (!albums.find((a) => a.cover == images[i].id)) {
       try {
-        await await imageService.deleteImage(images[i].id);
+        await imageService.deleteImageModel(images[i].id);
         logInfo(`Deleted image ${images[i].id}`, "Cover Cleaner");
       } catch (err) {
-        logError(err);
+        logError(
+          createFailure("DAO error", __filename, cleanAlbumlessImages.name, err)
+        );
+      }
+    }
+  }
+}
+
+async function cleanTinyLessFiles() {
+  try {
+    var images = await imageService.getAllImages();
+    var files = await fileService.getAllFiles(IMAGES_PATH);
+  } catch (err) {
+    logError(
+      createFailure(
+        "Cover cleaner error",
+        __filename,
+        cleanTinyLessFiles.name,
+        err
+      )
+    );
+  }
+
+  for (let i = 0; i < images.length; i++) {
+    if (!files.find((f) => f == images[i].tiny)) {
+      try {
+        imageService.deleteImageModel(images[i].id);
+        logInfo(`Deleted cover ${files[i]}`, "Cover Cleaner");
+      } catch (err) {
+        logError(
+          createFailure(
+            "Cover cleaner error",
+            __filename,
+            cleanTinyLessFiles.name,
+            err
+          )
+        );
       }
     }
   }
 }
 
 async function cleanTinylessImages() {
-  for (let i = 0; i < images.length; i++) {}
+  try {
+    var images = await imageService.getTinyLessImage();
+  } catch (err) {
+    throw createFailure("DAO err", __filename, cleanTinylessImages.name, err);
+  }
+
+  for (let i = 0; i < images.length; i++) {
+    try {
+      await imageService.deleteImageModel(images[i].id);
+    } catch (err) {
+      logError(
+        createFailure("DAO error", __filename, cleanTinylessImages.name, err)
+      );
+    }
+  }
+}
+
+async function cleanImageLessFiles() {
+  try {
+    var coverFiles = (await imageService.getAllImages())
+      .map((i) => [i.large, i.medium, i.path, i.small, i.tiny, i.verylarge])
+      .reduce((tab1, tab2) => [...tab1, ...tab2], [])
+      .filter((p) => p);
+    var files = await fileService.getAllFiles(IMAGES_PATH);
+  } catch (err) {
+    logError(
+      createFailure(
+        "Cover cleaner error",
+        __filename,
+        cleanImageLessFiles.name,
+        err
+      )
+    );
+  }
+
+  for (let i = 0; i < files.length; i++) {
+    if (!coverFiles.find((p) => p == files[i])) {
+      try {
+        await imageService.deleteImageFile(files[i]);
+        logInfo(`Deleted cover ${files[i]}`, "Cover Cleaner");
+      } catch (err) {
+        logError(
+          createFailure(
+            "Cover cleaner error",
+            __filename,
+            cleanImageLessFiles.name,
+            err
+          )
+        );
+      }
+    }
+  }
+}
+
+async function cleanAlbumCoverId() {
+  try {
+    var images = await imageService.getAllImages();
+    var albums = await albumService.getAllAlbums();
+  } catch (err) {
+    throw createFailure(
+      "DAO error",
+      __filename,
+      cleanAlbumlessImages.name,
+      err
+    );
+  }
+
+  try {
+    for (let i = 0; i < albums.length; i++) {
+      if (albums[i].cover && !images.find((im) => im.id == albums[i].cover)) {
+        albums[i].cover = null;
+        await albumService.updateAlbum(albums[i]);
+        logInfo(`Clean cover id for ${albums[i].id}`, "Cover Cleaner");
+      }
+    }
+  } catch (err) {
+    logError(
+      createFailure(
+        "Cover cleaner error",
+        __filename,
+        cleanAlbumCoverId.name,
+        err
+      )
+    );
+  }
+}
+
+async function cleanDeadFiles() {
+  try {
+    var images = await imageService.getAllImages();
+    var files = await fileService.getAllFiles(IMAGES_PATH);
+  } catch (err) {
+    throw createFailure("DAO err", __filename, cleanTinylessImages.name, err);
+  }
+
+  try {
+    for (let i = 0; i < images.length; i++) {
+      let isDead = ![
+        images[i].large,
+        images[i].medium,
+        images[i].small,
+        images[i].verylarge,
+      ]
+        .filter((i) => !!i)
+        .map((i) => !!files.find((f) => f == i))
+        .reduce((prev, cur) => prev && cur, true);
+
+      if (isDead) {
+        imageService.deleteImageModel(images[i].id);
+        logInfo(`Deleted image ${images[i].id}`, "Cover Cleaner");
+      }
+    }
+  } catch (err) {
+    logError(
+      createFailure("Cover cleaner error", __filename, cleanDeadFiles.name, err)
+    );
+  }
 }
 
 export default async function doWork() {
   logInfo("Cover cleaner started", "Cover Cleaner");
 
-  images = await imageService.getAllImages();
-  albums = await albumService.getAllAlbums();
-
-  await cleanAlbumlessImages();
-
-  images = await imageService.getTinyLessImage();
-  albums = await albumService.getAllAlbums();
+  try {
+    await cleanAlbumlessImages();
+    await cleanTinyLessFiles();
+    await cleanDeadFiles();
+    await cleanTinylessImages();
+    await cleanImageLessFiles();
+    await cleanAlbumCoverId();
+  } catch (err) {
+    logError(
+      createFailure("Cover cleaner error", __filename, doWork.name, err)
+    );
+  }
 }
--- src/workers/tasks/songCollector.ts
+++ src/workers/tasks/songCollector.ts
@@ -11,7 +11,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import fs from "fs/promises";
 import path from "path";
 import mm from "mime-types";
 import { MUSIC_PATH } from "../../config/config";
@@ -20,86 +19,62 @@ import { logError, logInfo } from "../../utils/logger";
 import { albumService } from "../../service/albumService";
 import { songService } from "../../service/songService";
 import { artistService } from "../../service/artistService";
+import { fileService } from "../../service/fileService";
 
 let songPaths: string[];
 let artists: Artist[];
 let albums: Album[];
 
-async function collect(libPath: string) {
+async function collect() {
   try {
-    var paths = await fs.readdir(libPath, { withFileTypes: true });
-  } catch (err) {
-    throw createFailure(err, __filename, collect.name);
-  }
+    let paths = await fileService.getAllFiles(MUSIC_PATH);
 
-  for (var i = 0; i < paths.length; i++) {
-    var fullPath = path.join(libPath, paths[i].name);
+    for (let i = 0; i < paths.length; i++) {
+      //Only considere audio files
+      if (!mm.lookup(path.extname(paths[i])).match("audio")) return;
 
-    if (paths[i].isDirectory()) {
-      try {
-        await collect(fullPath);
-      } catch (err) {
-        logError(err);
-        continue;
+      //If the song doesn't already exist, extract its metadata and create a new song
+      if (songPaths.find((p) => p == paths[i])) return;
+
+      let song = await songService.getMetadataFromFile(paths[i]);
+
+      logInfo(`Found new song ${song.path}`, "Song Collector");
+
+      //Fetch the song's album
+      let album = findAlbumByName(song.album, song.artist);
+      let albumId = album?.id;
+      if (!album) {
+        album = { artist: song.artist, title: song.album, year: song.year };
+
+        albumId = await albumService.createAlbum(album);
+
+        logInfo(`Found new album ${album.title}`, "Song Collector");
+
+        await updateAlbums();
       }
+
+      //Fetch the song's artist
+      let artist = findArtistByName(song.artist);
+      let artistId = artist?.id;
+      if (!artist) {
+        artist = { name: song.artist };
+
+        artistId = await artistService.createArtist(artist);
+
+        logInfo(`Found new artist ${artist.name}`, "Song Collector");
+
+        await updateArtists();
+      }
+
+      album.artistId = artistId;
+      await albumService.updateAlbum(album);
+
+      song.artistId = artistId;
+      song.albumId = albumId;
+      await songService.createSong(song);
     }
-
-    if (paths[i].isFile())
-      await registerSong(path.resolve(fullPath)).catch(() => {});
-  }
-}
-
-async function registerSong(songPath: string) {
-  try {
-    //Only considere audio files
-    if (!mm.lookup(path.extname(songPath)).match("audio")) return;
-
-    //If the song doesn't already exist, extract its metadata and create a new song
-    if (songPaths.find((p) => p == songPath)) return;
-
-    let song = await songService.getMetadataFromFile(songPath);
-
-    logInfo(`Found new song ${song.path}`, "Song Collector");
-
-    //Fetch the song's album
-    let album = findAlbumByName(song.album, song.artist);
-    let albumId = album?.id;
-    if (!album) {
-      album = { artist: song.artist, title: song.album, year: song.year };
-
-      albumId = await albumService.createAlbum(album);
-
-      logInfo(`Found new album ${album.title}`, "Song Collector");
-
-      await updateAlbums();
-    }
-
-    //Fetch the song's artist
-    let artist = findArtistByName(song.artist);
-    let artistId = artist?.id;
-    if (!artist) {
-      artist = { name: song.artist };
-
-      artistId = await artistService.createArtist(artist);
-
-      logInfo(`Found new artist ${artist.name}`, "Song Collector");
-
-      await updateArtists();
-    }
-
-    album.artistId = artistId;
-    await albumService.updateAlbum(album);
-
-    song.artistId = artistId;
-    song.albumId = albumId;
-    await songService.createSong(song);
   } catch (err) {
-    throw createFailure(
-      "Song collection error",
-      __filename,
-      registerSong.name,
-      err
-    );
+    throw createFailure("Song collection error", __filename, collect.name, err);
   }
 }
 
@@ -148,7 +123,7 @@ export default async function doWork() {
     await updateAlbums();
     await updateArtists();
 
-    await collect(MUSIC_PATH);
+    await collect();
   } catch (err) {
     logError(err);
   }
