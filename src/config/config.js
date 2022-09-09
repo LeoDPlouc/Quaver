b03ed4df55fd4ce612444fb9f2b9c2bb1b721e6b
LeoDplouc <LeoDplouc>
2022-09-09 16:22:40 +0200

    Removed express-session dependencie

--- src/config/config.js
+++ src/config/config.js
@@ -19,7 +19,6 @@ module.exports = {
     MONGO_PORT: Number(process.env.QUAVER_DB_PORT) || 27017,
     MONGO_USER: process.env.QUAVER_DB_USER,
     MONGO_PASSWORD: process.env.QUAVER_DB_PASSWORD,
-    SESSION_SECRET: process.env.QUAVER_SESSION_SECRET,
     APP_PORT: Number(process.env.QUAVER_PORT) || 8080,
     MUSIC_PATH: process.env.QUAVER_MUSIC_PATH || "/music",
     FPCALC_PATH: process.env.QUAVER_FPCALC_PATH,
--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -14,8 +14,8 @@
 import { Request, Response, NextFunction } from "express"
 
 function protect(req: Request, res: Response, next: NextFunction) {
-    const { user } = req.session
     /*
+    const { user } = req.session
         if (!user) {
             return res.status(401).json({ status: "fail", message: "unauthorized" })
         }
